fd1f787010f456e0ae8390d392cd90890751fbde
ShyGodB <qibingfang@gmail.com>
2020-04-27 23:33:26 +0800

    reset

--- api/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    ...require('./lib/common'),
-    format: require('./lib/format'),
-    crypto: require('./lib/crypto'),
-    ENV: require('./lib/ENV'),
-    url: require('./lib/url'),
-    client: require('./lib/client')
-}
